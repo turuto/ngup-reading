93f2d9ff6195fae2c355a568d9fef0900bca0d88
agariza <alberto.garcia@sivsa.com>
2019-11-29 12:14:32 +0100

    - First commit

---
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Stock } from '../../model/stock';
+
+@Component({
+  selector: 'app-stock-item',
+  templateUrl: './stock-item.component.html',
+  styleUrls: ['./stock-item.component.scss']
+})
+export class StockItemComponent implements OnInit {
+
+  public stock: Stock;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.stock = new Stock('Test Stock Company', 'TSC', 90, 80);
+  }
+
+  toggleFavorite(event) {
+    console.log('We are toggling the favorite state for this stock', event);
+    this.stock.favorite = !this.stock.favorite;
+  }
+
+}
